65a66142726968bedca835624226feb36e6c19d1
anpugliese <angellyp@uninorte.edu.co>
2020-03-17 00:24:14 +0100

    Bilinear Interpolation

--- calculate.py
+++ calculate.py
@@ -2,6 +2,15 @@ import numpy as np
 import isg_reader
 import isg_utils
 
+p = (-22.5, -68, 456)
+
 geoid = isg_reader.readfile("bolivia.isg")
 
-q1, q2, q3, q4 = isg_utils.getQuadrant(-22.5, -68, geoid)
+q1, q2, q3, q4 = isg_utils.getQuadrant(p, geoid)
+
+Np = isg_utils.interpolation(p, q1, q2, q3, q4, geoid, 'bilinear')
+print(Np)
+
+H = isg_utils.orthometric_height(p, Np)
+
+print(H)
--- isg_utils.py
+++ isg_utils.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-def getQuadrant(lat, lng, geoid):
+def getQuadrant(p, geoid):
     min_lat = geoid['latmin']
     delta_lat = geoid['deltalat']
     min_lng = geoid['lonmin']
     delta_lng = geoid['deltalon']
-    
+    lat = p[0]
+    lng = p[1]
+
     i_up = int((lat - min_lat) / delta_lat)
     if i_up == geoid['nrows']: i_up = i_up - 1
     i_down = i_up + 1
@@ -15,3 +17,42 @@ def getQuadrant(lat, lng, geoid):
     j_right = j_left + 1
     
     return (i_up, j_left), (i_up, j_right), (i_down, j_left), (i_down, j_right)
+
+
+def interpolation(p, p1, p2, p3, p4, geoid, type='bilinear'):
+    min_lat = geoid['latmin']
+    delta_lat = geoid['deltalat']
+    min_lng = geoid['lonmin']
+    delta_lng = geoid['deltalon']
+    grid = geoid['grid']
+
+   
+    lat1 = min_lat + delta_lat*p1[0]
+    lng1 = min_lng + delta_lng*p1[1]
+    lat2 = min_lat + delta_lat*p2[0]
+    lng2 = min_lng + delta_lng*p2[1]
+    lat3 = min_lat + delta_lat*p3[0]
+    lng3 = min_lng + delta_lng*p3[1]
+    lat4 = min_lat + delta_lat*p4[0]
+    lng4 = min_lng + delta_lng*p4[1]
+
+
+
+    if type == 'bilinear': 
+        A = np.array([[lat1, lng1, lat1*lng1, 1],
+                      [lat2, lng2, lat2*lng2, 1], 
+                      [lat3, lng3, lat3*lng3, 1],
+                      [lat4, lng4, lat4*lng4, 1]]) 
+
+        B = np.array([grid[p1], grid[p2], grid[p3], grid[p4]]) 
+        x = np.linalg.solve(A, B) 
+        print(x)
+
+        Np = x[0]*p[0]+ x[1]*p[1] + x[2]*p[0]*p[1] + x[3]
+
+        return Np
+
+
+def orthometric_height(p, Np):
+    return p[2] - Np
+
